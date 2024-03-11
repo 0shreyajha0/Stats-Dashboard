997e8ae8eae0c7489a3df84239c6172e1f44e85b
0shreyajha0 <0shreyajha0@gmail.com>
2024-03-11 17:09:13 +0530

    updated SessionCard

--- src/components/dashboard/ResponsiveAreaChart.tsx
+++ src/components/dashboard/ResponsiveAreaChart.tsx
@@ -59,26 +59,10 @@ export const ResponsiveAreaChart = ({ data }: TResponsiveAreaChartProps) => {
         />
         <div className="collapse-title ">
           <div className="flex pb-4">
-            <SessionCard
-              cardName={"Online store session"}
-              amount={"255,581"}
-              rate={"9"}
-            />{" "}
-            <SessionCard
-              cardName={"Net Return Value"}
-              amount={"-$15,07.44"}
-              rate={"14"}
-            />{" "}
-            <SessionCard
-              cardName={" Total Orders"}
-              amount={"10,511"}
-              rate={"2"}
-            />{" "}
-            <SessionCard
-              cardName={" Conversion Rate"}
-              amount={"3.18"}
-              rate={"7"}
-            />
+            <SessionCard cardName={"Online store session"} />{" "}
+            <SessionCard cardName={"Net Return Value"} />{" "}
+            <SessionCard cardName={" Total Orders"} />{" "}
+            <SessionCard cardName={" Conversion Rate"} />
           </div>
         </div>
         <div className="collapse-content">
